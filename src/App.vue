e22d0a103e66268178871b9bbc8bc1b32b26e666
LAPTOP-EHDI9A5M\sarat <sarath@armino.in>
2022-07-24 12:18:38 +0530

    removed

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
     >
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
+          alt="Vuetify Logo" 
           class="shrink mr-2"
           contain
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
